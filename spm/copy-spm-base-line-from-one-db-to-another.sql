42e58700c32d53165d9bc6b84d5e6d1ac650a088
Oracle-Hub <49007293+Oracle-Hub@users.noreply.github.com>
2024-02-14 22:43:11 -0600

    Create copy-spm-base-line-from-one-db-to-another.sql

---
+++ spm/copy-spm-base-line-from-one-db-to-another.sql
@@ -0,0 +1,62 @@
+
+-------
+Crate staging table
+
+BEGIN
+DBMS_SPM.CREATE_STGTAB_BASELINE(
+table_name => 'spm_9g9rv13y443s2',
+table_owner => 'XXIRIS',
+tablespace_name => 'APPS_TS_TX_DATA');
+END;
+/
+
+
+---- Load plan into Staging table 
+  
+SET SERVEROUTPUT ON
+DECLARE
+l_plans_packed PLS_INTEGER;
+BEGIN
+l_plans_packed := DBMS_SPM.pack_stgtab_baseline(
+table_name => 'spm_9g9rv13y443s2',
+table_owner => 'XXIRIS',
+sql_handle => 'SQL_b965bedab0024296',
+plan_name => 'SQL_PLAN_bktdyvas04hnq0743672e');
+DBMS_OUTPUT.put_line('Plans Packed: ' || l_plans_packed);
+END;
+/
+
+
+-----Export staging table
+expdp  tables=XXIRIS.spm_9g9rv13y443s2 directory=SQLTS dumpfile=spm_9g9rv13y443s2.dmp logfile=spm_9g9rv13y443s2.log
+
+
+----------On Target 
+col OWNER for a15 
+col DIRECTORY_NAME for a30
+col DIRECTORY_PATH for a100
+set line 200
+
+select * from dba_directories where directory_name='SQLTS';
+
+--- Import staging table ----- 
+
+impdp full=y directory=SQLTS dumpfile=spm_9g9rv13y443s2.dmp logfile=spm_9g9rv13y443s2.log
+
+
+----- Load SPM details into database -------------------
+
+SET SERVEROUTPUT ON
+DECLARE
+l_plans_unpacked PLS_INTEGER;
+BEGIN
+l_plans_unpacked := DBMS_SPM.unpack_stgtab_baseline(
+table_name => 'spm_9g9rv13y443s2',
+table_owner => 'XXIRIS');
+
+DBMS_OUTPUT.put_line('Plans Unpacked: ' || l_plans_unpacked);
+END;
+/
+
+
+
